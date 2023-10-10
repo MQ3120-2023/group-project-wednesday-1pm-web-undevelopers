c21bb5a9d88aa4d4499d3641f81b93aa685e9e7a
Timothy <timothy.chanata@students.mq.edu.au>
2023-10-10 19:12:55 +1100

    able to pull from api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import RecipeList from './components/RecipeList';
+import logo from "./logo.svg";
+import "./App.css";
+import RecipeList from "./components/RecipeList";
 
-import axios from 'axios'
+import axios from "axios";
 import React, { useState } from "react";
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
-
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Home = () => {
-  return(
+  return (
     <div>
       <h1>Welcome we show food here</h1>
+      <RecipeList />
     </div>
   );
-
-}
+};
 
 const Menu = () => {
   return (
     <div>
-      <h1>Recipes</h1>
-      <RecipeList searchQuery="chicken" /> {/* Provide the default search query or an empty string */}
+      <h1>Idk how to fetch the api yet</h1>
     </div>
   );
 };
@@ -29,25 +26,25 @@ const Menu = () => {
 function App() {
   return (
     <div className="App">
-    <Router>
-      <nav className="navbar">
-        <h1>Our food shower</h1>
-        <div className="nav-links">
-          <Link className="nav-link" to="/">Home</Link>
-          <Link className="nav-link" to="/menu">Menu</Link>
-        </div>
-
-
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/menu" element={<Menu/>} />
-      </Routes>
-    </Router>
+      <Router>
+        <nav className="navbar">
+          <h1>Our food shower</h1>
+          <div className="nav-links">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/menu">
+              Menu
+            </Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+      </Router>
     </div>
   );
-
-
 }
 
 export default App;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const RecipeList = ({ searchQuery }) => {
+const RecipeList = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const response = await axios.get(
-          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
         );
         setRecipes(response.data.meals);
       } catch (error) {
@@ -16,16 +17,21 @@ const RecipeList = ({ searchQuery }) => {
       }
     };
 
-    // Fetch recipes when searchQuery changes
     fetchRecipes();
-  }, [searchQuery]);
+  }, [searchTerm]);
 
   return (
     <div>
-      <h2>Recipe List</h2>
+      <h1>Recipes</h1>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search for recipes..."
+      />
       {recipes.map((recipe) => (
         <div key={recipe.idMeal}>
-          <h3>{recipe.strMeal}</h3>
+          <h2>{recipe.strMeal}</h2>
           <img src={recipe.strMealThumb} alt={recipe.strMeal} />
           {/* Add more details as needed */}
         </div>
