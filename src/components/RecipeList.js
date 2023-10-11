0f5d16bdbae3c7fb2de7a92ffb613f3defbeddc4
Timothy <timothy.chanata@students.mq.edu.au>
2023-10-11 14:12:59 +1100

    put back css links... my bad

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "../styling/RecipeList.css";
+
 
 const RecipeList = () => {
   const [searchTerm, setSearchTerm] = useState("");
