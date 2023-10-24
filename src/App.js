9a9f325bd4ce3d4772e48d68e7858d69ec47e01d
nobodywat <aaron.ngo@students.mq.edu.au>
2023-10-24 22:16:15 +1100

    cleaning code up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import IngredientSearch from "./components/IngredientSearch";
 import AddToFavorites from "./functions/addToFavorites";
-import Login from "./components/login";
+import Home from "./components/Home";
 
 import axios from "axios";
 import React, { useState } from "react";
@@ -19,15 +19,10 @@ function App() {
             <img src={logo} alt="MEALBROS" className="logo" />
           </Link>
           <div className="nav-links">
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
-            <Link className="nav-link" to="/ingredient-search">
-              Ingredient Search
-            </Link>
-            <Link className="nav-link" to="/recipe-search">
-              Recipe Search
-            </Link>
+            <Link className="nav-link" to="/">Home</Link>
+            <Link className="nav-link" to="/recipe-search">Recipe Search</Link>
+            <Link className="nav-link" to="/ingredient-search">Ingredient Search</Link>
+            <Link className="nav-link" to="/favorites">Favorites</Link>
           </div>
         </nav>
 
@@ -43,47 +38,5 @@ function App() {
   );
 }
 
-const Home = () => {
-  return (
-    <div className="home-container">
-      <h1>Welcome to MealBro's!</h1>
-      <Login />
-      <section className="about-section">
-        <h2>About Us</h2>
-        <p>
-          Mealbros is a fantastic app that helps you discover and organize your
-          favorite recipes. Whether you're a cooking enthusiast or a beginner,
-          Mealbros is here to make your cooking experience enjoyable and
-          hassle-free.
-        </p>
-        <img
-          src="https://picsum.photos/1000/400"
-          alt="Delicious Meal"
-          className="about-image"
-        />
-      </section>
-      <section className="features-section">
-        <h2>Key Features</h2>
-        <div className="feature">
-          <img src="https://picsum.photos/1000/250" alt="Feature 1" />
-          <p>Explore a vast collection of recipes from various cuisines.</p>
-        </div>
-        <div className="feature">
-          <img src="https://picsum.photos/1000/200" alt="Feature 2" />
-          <p>Search for recipes based on ingredients you have at home.</p>
-        </div>
-      </section>
-      <section className="how-it-works-section">
-        <h2>How It Works</h2>
-        <p>
-          Mealbros uses advanced algorithms to recommend recipes based on your
-          preferences. Simply search for recipes, save your favorites, and enjoy
-          cooking delicious meals.
-        </p>
-      </section>
-      <footer>Created by The Undevelopers</footer>
-    </div>
-  );
-};
 
 export default App;
