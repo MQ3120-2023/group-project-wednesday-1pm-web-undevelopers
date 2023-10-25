9eaa60df78b9cd1a44b7874b2c21e0d15d9ea96d
nobodywat <aaron.ngo@students.mq.edu.au>
2023-10-25 22:25:49 +1100

    empty favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,10 +1,6 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+import React from 'react';
 import "../styling/Favourites.css";
-
 import { removeFavorite } from '../functions/favoriteFunctions';
-
-
 
 export default function Favorites({favorites}) {
 
@@ -12,13 +8,19 @@ export default function Favorites({favorites}) {
         <div>
             <h1>Favourites</h1>
             <div className="favorites-container">
-                {favorites.map((fav) => (
-                    <div key={fav.id} className="fav-card">
-                        <img src={fav.strMealThumb} alt={fav.strMeal} className="fav-image" />
-                        <h3 className="fav-name">{fav.strMeal}</h3>
-                        <button className="fav-button" onClick={() => {removeFavorite(fav.id)}}>Remove</button>      
+                {favorites.length === 0 ? (
+                    <div className="emptyFavsList">
+                        <p>There is nothing in the favorites list.</p>
                     </div>
-                ))}
+                ) : (
+                    favorites.map((fav) => (
+                        <div key={fav.id} className="fav-card">
+                            <img src={fav.strMealThumb} alt={fav.strMeal} className="fav-image" />
+                            <h3 className="fav-name">{fav.strMeal}</h3>
+                            <button className="fav-button" onClick={() => {removeFavorite(fav.id)}}>Remove</button>      
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     )
