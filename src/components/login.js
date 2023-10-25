09d68ae8bc52f10d4d423651a57495bf6c3b9e97
aroosharun <79824683+aroosharun@users.noreply.github.com>
2023-10-25 13:50:05 +1100

    Added css to logout button

--- src/components/login.js
+++ src/components/login.js
@@ -41,7 +41,7 @@ const Login = () => {
       return (
         <div>
           <h1>Welcome, {username}!</h1>
-          <button onClick={handleLogout}>Logout</button> 
+          <button className="login-button" onClick={handleLogout}>Logout</button> 
         </div>
       );
     }
