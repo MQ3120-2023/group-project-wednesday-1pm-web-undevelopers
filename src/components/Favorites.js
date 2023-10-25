0380072a0558aed5fe4cfc9d6e4c2e016faaf056
aroosharun <79824683+aroosharun@users.noreply.github.com>
2023-10-25 22:45:35 +1100

    Centred "nothing in favourites list" text

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -6,22 +6,25 @@ export default function Favorites({favorites}) {
 
     return (
         <div>
-            <h1>Favourites</h1>
-            <div className="favorites-container">
-                {favorites.length === 0 ? (
-                    <div className="emptyFavsList">
-                        <p>There is nothing in the favorites list.</p>
-                    </div>
-                ) : (
-                    favorites.map((fav) => (
-                        <div key={fav.id} className="fav-card">
-                            <img src={fav.strMealThumb} alt={fav.strMeal} className="fav-image" />
-                            <h3 className="fav-name">{fav.strMeal}</h3>
-                            <button className="fav-button" onClick={() => {removeFavorite(fav.id)}}>Remove</button>      
-                        </div>
-                    ))
-                )}
+          <h1>Favourites</h1>
+          {favorites.length === 0 ? (
+            <div className="emptyFavsList">
+              <p>There is nothing in the favorites list.</p>
             </div>
+          ) : (
+            <div className="favorites-container">
+              {favorites.map((fav) => (
+                <div key={fav.id} className="fav-card">
+                  <img src={fav.strMealThumb} alt={fav.strMeal} className="fav-image" />
+                  <h3 className="fav-name">{fav.strMeal}</h3>
+                  <button className="fav-button" onClick={() => {removeFavorite(fav.id)}}>
+                    Remove
+                  </button>
+                </div>
+              ))
+            }
+          </div>
+        )}
         </div>
     )
 }
