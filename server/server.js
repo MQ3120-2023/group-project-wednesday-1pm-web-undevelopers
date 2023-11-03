ad6514772c159398a01e3bfc48c16457e8100834
nobodywat <aaron.ngo@students.mq.edu.au>
2023-11-03 17:38:53 +1100

    beginning deployment
    created the build and test it ran

--- server/server.js
+++ server/server.js
@@ -2,21 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const path = require('path');
 const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const buildPath = path.join(__dirname, 'build')
-app.use(express.static(buildPath))
+app.use(express.static('build'))
+
 
 // Route for the root endpoint
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to the backend</h1>");
 });
 
+// gets the static files from the build folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+})
+
 // Setting up the server to listen on a specific port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
